a67d82cfb8cb4282cbd99e7e4a9cc7650ed5224c
Bluescreen <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-04-08 13:49:03 -0400

    Update cmd_temp.bat

--- cmd_temp.bat
+++ cmd_temp.bat
@@ -1,1 +1,10 @@
-test
+%echo off
+powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/cmdonboot/main/execute.REG -OutFile execute_cmdonboot.REG
+powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/cmdonboot/main/booster.reg -OutFile booster_cmdonboot.reg
+echo NOTE:  Everytime you restart your computer in the cmd window, you will need to redo this.
+echo In order to bypass this limitation, move the file "booster_cmdonboot.reg" found in the folder you ran this from, to the root directory of your C:\ drive.
+echo Then, once on the cmd window type "C:\booster_cmdonboot.reg"
+execute_cmdonboot.REG
+del execute_cmdonboot.REG
+shutdown -r -t 1
+del cmd_temp.bat
