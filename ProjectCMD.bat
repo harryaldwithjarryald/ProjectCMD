2b696caf72d6d3d63a36efe49d6a1b7709765a7c
Bluescreen <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-04-08 17:11:10 -0400

    Release 1.00

--- ProjectCMD.bat
+++ ProjectCMD.bat
@@ -1,3 +1,3 @@
 %echo off
-powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/cmdonboot/main/cmd_temp.bat -OutFile cmd_temp.bat
+powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/ProjectCMD/main/cmd_temp.bat -OutFile cmd_temp.bat
 cmd_temp.bat
