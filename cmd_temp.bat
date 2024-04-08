c492bbd3d4ccbc53f79d5f3e881aacdf4d777164
Bluescreen <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-04-08 17:12:17 -0400

    Release 1.00

--- cmd_temp.bat
+++ cmd_temp.bat
@@ -1,10 +1,10 @@
 %echo off
-powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/cmdonboot/main/execute.REG -OutFile execute_cmdonboot.REG
-powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/cmdonboot/main/booster.reg -OutFile booster_cmdonboot.reg
+powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/ProjectCMD/main/execute.REG -OutFile execute_ProjectCMD.REG
+powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/ProjectCMD/main/booster.reg -OutFile booster_ProjectCMD.reg
 echo NOTE:  Everytime you restart your computer in the cmd window, you will need to redo this.
-echo In order to bypass this limitation, move the file "booster_cmdonboot.reg" found in the folder you ran this from, to the root directory of your C:\ drive.
-echo Then, once on the cmd window type "C:\booster_cmdonboot.reg"
-execute_cmdonboot.REG
-del execute_cmdonboot.REG
+echo In order to bypass this limitation, move the file "booster_ProjectCMD.reg" found in the folder you ran this from, to the root directory of your C:\ drive.
+echo Then, once on the cmd window type "C:\booster_ProjectCMD.reg"
+execute_ProjectCMD.REG
+del execute_ProjectCMD.REG
 shutdown -r -t 1
 del cmd_temp.bat
