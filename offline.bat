a0ac93411c89abeac1cd0935623fcc19be184ce3
harryaldwithjarryald <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-04-24 15:01:27 -0400

    Fixed - 1.00

--- offline.bat
+++ offline.bat
@@ -2,7 +2,6 @@
 echo NOTE:  Everytime you restart your computer in the cmd window, you will need to redo this.
 echo In order to bypass this limitation, move the file "booster_cmdonboot.reg" found in the folder you ran this from, to the root directory of your C:\ drive.
 echo Then, once on the cmd window type "C:\booster_cmdonboot.reg"
-execute_cmdonboot.REG
-del execute_cmdonboot.REG
+execute.REG
+del execute.REG
 shutdown -r -t 1
-del offline.bat
