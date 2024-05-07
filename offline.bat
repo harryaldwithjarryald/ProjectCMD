0a0ab333a19f4d96196a4221981996c058ce1c48
harryaldwithjarryald <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-05-06 22:57:49 -0400

    Release 1.10

--- offline.bat
+++ offline.bat
@@ -1,4 +1,4 @@
-%echo off
+@echo off
 echo NOTE:  Everytime you restart your computer in the cmd window, you will need to redo this.
 echo In order to bypass this limitation, move the file "booster.reg" found in the folder you ran this from, to the root directory of your C:\ drive.
 echo Then, once on the cmd window type "C:\booster.reg"
