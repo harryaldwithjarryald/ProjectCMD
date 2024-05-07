0899820a896ea2673f83359b904b5e831f3969fc
harryaldwithjarryald <148587892+harryaldwithjarryald@users.noreply.github.com>
2024-05-06 22:56:28 -0400

    Release 1.10

--- cmd_temp.bat
+++ cmd_temp.bat
@@ -1,10 +1,10 @@
 %echo off
-powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/ProjectCMD/main/execute.REG -OutFile execute_ProjectCMD.REG
+powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/ProjectCMD/main/execute.reg -OutFile execute_ProjectCMD.reg
 powershell /c wget https://raw.githubusercontent.com/harryaldwithjarryald/ProjectCMD/main/booster.reg -OutFile booster_ProjectCMD.reg
 echo NOTE:  Everytime you restart your computer in the cmd window, you will need to redo this.
 echo In order to bypass this limitation, move the file "booster_ProjectCMD.reg" found in the folder you ran this from, to the root directory of your C:\ drive.
 echo Then, once on the cmd window type "C:\booster_ProjectCMD.reg"
-execute_ProjectCMD.REG
-del execute_ProjectCMD.REG
+execute_ProjectCMD.reg
+del execute_ProjectCMD.reg
 shutdown -r -t 1
 del cmd_temp.bat
